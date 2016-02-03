fb03c33ae3c3455887dc9971f7e532bdb8256df3
abcdefghi99999@mailinator.com <abcdefghi99999@mailinator.com>
2016-02-03 11:51:20 +0000

    Init

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private new Transform transform;
+    private new Rigidbody rigidbody;
+    private new AudioSource audio;
+    void Awake()
+    {
+        transform = GetComponent<Transform>(); 
+        rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+}
